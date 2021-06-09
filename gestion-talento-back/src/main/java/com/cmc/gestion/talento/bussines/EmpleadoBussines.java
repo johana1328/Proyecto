3752ac744cb016c0bc1c04b46bbf09baf1225745
mario mendez <mendezmartinezmario@gmail.com>
2021-06-09 09:14:26 -0500

    creacion de usuario

--- gestion-talento-back/src/main/java/com/cmc/gestion/talento/bussines/EmpleadoBussines.java
+++ gestion-talento-back/src/main/java/com/cmc/gestion/talento/bussines/EmpleadoBussines.java
@@ -1,5 +1,8 @@
 package com.cmc.gestion.talento.bussines;
 
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,12 +11,39 @@ import org.springframework.stereotype.Service;
 import com.cmc.gestion.talento.bussines.dto.EmpleadoDto;
 import com.cmc.gestion.talento.jpa.dao.EmpleadoDao;
 import com.cmc.gestion.talento.jpa.entity.Empleado;
+import com.cmc.gestion.talento.jpa.type.TipoArea;
+import com.cmc.gestion.talento.jpa.type.TipoDocumento;
+import com.cmc.gestion.talento.jpa.type.TipoEstadoUsuario;
+import com.cmc.gestion.talento.jpa.type.TipoPerfil;
 
 @Service
 public class EmpleadoBussines {
 	
 	@Autowired
 	private EmpleadoDao empleadoDao;
+	
+	
+	public void crearEmpleado(EmpleadoDto empleado) {
+		Empleado empleadoEntity = new Empleado();
+		empleadoEntity.setArea(empleado.getArea());
+		empleadoEntity.setJefe(empleado.getJefe());
+		empleadoEntity.setIdUsuario(empleado.getIdUsuario());
+		empleadoEntity.setDocumento(empleado.getDocumento());
+		empleadoEntity.setTipoDocumento(empleado.getTipoDocumento());
+		empleadoEntity.setPrimerNombre(empleado.getPrimerNombre());
+		empleadoEntity.setSegundoNombre(empleado.getSegundoNombre());
+		empleadoEntity.setPrimerApellido(empleado.getPrimerApellido());
+		empleadoEntity.setSegundoApellido(empleado.getSegundoApellido());
+		empleadoEntity.setTelefono(empleado.getTelefono());
+		empleadoEntity.setCorreoElectronico(empleado.getCorreoElectronico());
+		empleadoEntity.setEstado(TipoEstadoUsuario.CREADO);
+		empleadoEntity.setFechaCreacion(Calendar.getInstance());
+		empleadoEntity.setFechaActualizacion(Calendar.getInstance());
+		List<TipoPerfil> perfilUsuario = Arrays.asList(new TipoPerfil[] {empleado.getPerfil()});
+		empleadoEntity.setPerfilUsuario(perfilUsuario);
+		empleadoEntity.setContrasena("1234");
+		empleadoDao.save(empleadoEntity);
+	}
 	
 	
 	public List<EmpleadoDto> getAllEmpleados(){
