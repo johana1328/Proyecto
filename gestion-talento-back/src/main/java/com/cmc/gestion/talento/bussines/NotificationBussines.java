1833a2b73294c9233523599390db3261122a8fbe
johana1328 <johanakatherin05@gmail.com>
2021-06-22 06:27:58 -0500

    Metodos mensajeria

--- gestion-talento-back/src/main/java/com/cmc/gestion/talento/bussines/NotificationBussines.java
+++ gestion-talento-back/src/main/java/com/cmc/gestion/talento/bussines/NotificationBussines.java
@@ -38,7 +38,7 @@ public class NotificationBussines {
 		
 		NotificationClinetDto notificacion = new NotificationClinetDto();
 		notificacion.setCorreoDestinatario(destinatario);
-		notificacion.setAsunto("Creacion de usuario");
+		notificacion.setAsunto("Creación de usuario");
 		notificacion.setTemplate("T004");
 		notificacion.setTemplateLayout("NORMAL");
 		List<Parametro> parametros = new ArrayList<>();
@@ -88,7 +88,98 @@ public class NotificationBussines {
 		sendNotification(notificacion);  
 	} 
 	
+	public void inscripcionCandidato(String destinatario) {
+		NotificationClinetDto notificacion = new NotificationClinetDto();
+		notificacion.setCorreoDestinatario(destinatario);
+		notificacion.setAsunto("¡Tu inscripción ha sido exitosa! ");
+		notificacion.setTemplate("T007");
+		notificacion.setTemplateLayout("GENERAL");
+		List<Attachment> listAttachment = new ArrayList<>();
+		Attachment attachment = new Attachment();
+		attachment.setName("inscripcion");
+		attachment.setPath("images/inscripcion.jpg");
+		attachment.setType("INLINE");
+		listAttachment.add(attachment);
+		notificacion.setAttachment(listAttachment);
+		sendNotification(notificacion);
+	}
 	
+	public void activacionPruebaTecnica(String destinatario, String nombre) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		SimpleDateFormat hourFormat = new SimpleDateFormat(HOURS_FORMAT);
+		String fecha = dateFormat.format(new Date());
+		String hora = hourFormat.format(new Date());
+		NotificationClinetDto notificacion = new NotificationClinetDto();
+		notificacion.setCorreoDestinatario(destinatario);
+		notificacion.setAsunto("¡Tu activación está lista! ");
+		notificacion.setTemplate("T001");
+		notificacion.setTemplateLayout("GENERAL");
+		List<Parametro> parametros = new ArrayList<>();
+		parametros.add(new Parametro("fecha", fecha));
+		parametros.add(new Parametro("hora", hora));
+		parametros.add(new Parametro("nombre", nombre));
+		parametros.add(new Parametro("aplicativo", env.getProperty("com.cmc.ingreso.url")));
+		notificacion.setParametros(parametros);
+		List<Attachment> listAttachment = new ArrayList<>();
+		Attachment attachment = new Attachment();
+		attachment.setName("tecnica");
+		attachment.setPath("images/tecnica.jpg");
+		attachment.setType("INLINE");
+		listAttachment.add(attachment);
+		notificacion.setAttachment(listAttachment);
+		sendNotification(notificacion);  
+	}
+	
+	public void activacionPruebaPsitecnica(String destinatario, String nombre) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		SimpleDateFormat hourFormat = new SimpleDateFormat(HOURS_FORMAT);
+		String fecha = dateFormat.format(new Date());
+		String hora = hourFormat.format(new Date());
+		NotificationClinetDto notificacion = new NotificationClinetDto();
+		notificacion.setCorreoDestinatario(destinatario);
+		notificacion.setAsunto("¡Tu activación está lista! ");
+		notificacion.setTemplate("T002");
+		notificacion.setTemplateLayout("GENERAL");
+		List<Parametro> parametros = new ArrayList<>();
+		parametros.add(new Parametro("fecha", fecha));
+		parametros.add(new Parametro("hora", hora));
+		parametros.add(new Parametro("nombre", nombre));
+		parametros.add(new Parametro("aplicativo", env.getProperty("com.cmc.ingreso.url")));
+		notificacion.setParametros(parametros);
+		List<Attachment> listAttachment = new ArrayList<>();
+		Attachment attachment = new Attachment();
+		attachment.setName("psicotecnica");
+		attachment.setPath("images/psicotecnica.jpg");
+		attachment.setType("INLINE");
+		listAttachment.add(attachment);
+		notificacion.setAttachment(listAttachment);
+		sendNotification(notificacion);  
+	}
+	public void activacionEvaluacion(String destinatario, String nombre) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		SimpleDateFormat hourFormat = new SimpleDateFormat(HOURS_FORMAT);
+		String fecha = dateFormat.format(new Date());
+		String hora = hourFormat.format(new Date());
+		NotificationClinetDto notificacion = new NotificationClinetDto();
+		notificacion.setCorreoDestinatario(destinatario);
+		notificacion.setAsunto("¡Tu activación está lista! ");
+		notificacion.setTemplate("T003");
+		notificacion.setTemplateLayout("GENERAL");
+		List<Parametro> parametros = new ArrayList<>();
+		parametros.add(new Parametro("fecha", fecha));
+		parametros.add(new Parametro("hora", hora));
+		parametros.add(new Parametro("nombre", nombre));
+		parametros.add(new Parametro("aplicativo", env.getProperty("com.cmc.ingreso.url")));
+		notificacion.setParametros(parametros);
+		List<Attachment> listAttachment = new ArrayList<>();
+		Attachment attachment = new Attachment();
+		attachment.setName("edesempeno");
+		attachment.setPath("images/edesempeno.jpg");
+		attachment.setType("INLINE");
+		listAttachment.add(attachment);
+		notificacion.setAttachment(listAttachment);
+		sendNotification(notificacion);  
+	}
 public void xxxxxx(String destinatario) {
 		NotificationClinetDto notificacion = new NotificationClinetDto();
 		notificacion.setCorreoDestinatario(destinatario);
