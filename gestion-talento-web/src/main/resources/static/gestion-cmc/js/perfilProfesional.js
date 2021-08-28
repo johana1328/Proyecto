823b725ae9e67f5dfb042b0ff9a02d4818d8bef8
villa942 <villajuanmora@gmail.com>
2021-08-28 14:43:41 -0500

    validacion de candidato, experiencia,perfil y tecnicas incompleto

---
+++ gestion-talento-web/src/main/resources/static/gestion-cmc/js/perfilProfesional.js
@@ -0,0 +1,34 @@
+var form = $("#formPerfilPro");
+
+function validacionFormularioE(){
+	form.validate({
+		 errorContainer: "#messageBox1",
+         errorLabelContainer: "#messageBox1 ul",
+         wrapper: "li",
+		rules:{
+			perfilPro:{
+				required: true
+			
+			},
+			experiencia:{
+			required: true 
+			
+			},
+			
+       },
+		messages:{
+			perfilPro:{
+				required: "El campo perfil profesional es requerido"
+			
+			},
+			experiencia:{
+			required: "El campo experiencia profesional es requerido"
+			
+			},
+		}
+	}).settings.ignore = ":disabled,:hidden";
+	
+	if (form.valid() == true){
+		stepper.next();
+	}
+}
