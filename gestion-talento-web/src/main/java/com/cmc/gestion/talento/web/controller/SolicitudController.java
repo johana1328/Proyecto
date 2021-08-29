0379dc346903ba24d08179b11919229c1d20f332
mario mendez <mendezmartinezmario@gmail.com>
2021-08-29 11:45:23 -0500

    ajustes

--- gestion-talento-web/src/main/java/com/cmc/gestion/talento/web/controller/SolicitudController.java
+++ gestion-talento-web/src/main/java/com/cmc/gestion/talento/web/controller/SolicitudController.java
@@ -1,18 +1,34 @@
 package com.cmc.gestion.talento.web.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cmc.gestion.talento.bussines.TarifaBussines;
+import com.cmc.gestion.talento.bussines.dto.TarifaDto;
+
 @Controller
 @RequestMapping("/administracion/solicitud")
 public class SolicitudController {
+	
+	@Autowired
+	private TarifaBussines tarifaBussines;
 	
 	@GetMapping
 	public String init(Model model) {
 		model.addAttribute("Errror", "NOK");
 		return "pages/administracion/peticiones/solicitud";
+	}
+	
+	@GetMapping("/crear")
+	public String viewCrear(Model model) {
+		List<TarifaDto> tarifas=tarifaBussines.getAllTarifa();
+		model.addAttribute("tarifas", tarifas);
+		return "pages/administracion/peticiones/crearSP";
 	}
 
 }
