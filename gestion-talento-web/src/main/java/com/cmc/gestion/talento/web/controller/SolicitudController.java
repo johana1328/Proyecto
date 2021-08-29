da089bbe4f6b2123009a22251ae48fbd4392ab34
johana1328 <johanakatherin05@gmail.com>
2021-08-29 12:17:35 -0500

    Cambio Select solicitud

--- gestion-talento-web/src/main/java/com/cmc/gestion/talento/web/controller/SolicitudController.java
+++ gestion-talento-web/src/main/java/com/cmc/gestion/talento/web/controller/SolicitudController.java
@@ -8,8 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.cmc.gestion.talento.bussines.AmbientacionBussines;
+import com.cmc.gestion.talento.bussines.EspecialidadBussines;
 import com.cmc.gestion.talento.bussines.TarifaBussines;
+import com.cmc.gestion.talento.bussines.TipoPerfilBussines;
+import com.cmc.gestion.talento.bussines.dto.AmbientacionDto;
+import com.cmc.gestion.talento.bussines.dto.EspecialidadDto;
 import com.cmc.gestion.talento.bussines.dto.TarifaDto;
+import com.cmc.gestion.talento.bussines.dto.TipoPerfilDto;
 
 @Controller
 @RequestMapping("/administracion/solicitud")
@@ -17,6 +23,15 @@ public class SolicitudController {
 	
 	@Autowired
 	private TarifaBussines tarifaBussines;
+	
+	@Autowired
+	private AmbientacionBussines ambientacionBussines;
+	
+	@Autowired
+	private TipoPerfilBussines tipoPerfilBussines;
+	
+	@Autowired
+	private EspecialidadBussines especialidadBussines;
 	
 	@GetMapping
 	public String init(Model model) {
@@ -27,7 +42,13 @@ public class SolicitudController {
 	@GetMapping("/crear")
 	public String viewCrear(Model model) {
 		List<TarifaDto> tarifas=tarifaBussines.getAllTarifa();
+		List<EspecialidadDto> especialidades = especialidadBussines.getAllEspecialidad();
+		List<TipoPerfilDto> perfiles = tipoPerfilBussines.getAllTipoPerfil();
+		List<AmbientacionDto> ambientaciones = ambientacionBussines.getAllAmbientacion();
 		model.addAttribute("tarifas", tarifas);
+		model.addAttribute("especialidades", especialidades);
+		model.addAttribute("perfiles", perfiles);
+		model.addAttribute("ambientaciones", ambientaciones);
 		return "pages/administracion/peticiones/crearSP";
 	}
 
